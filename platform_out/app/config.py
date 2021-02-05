02700e66f3bd924babd311528806feba21cf7648
Sheena Puthanpurayil <sheena24789@gmail.com>
2021-02-05 14:53:33 +0200

    cleanup

--- platform_out/app/config.py
+++ platform_out/app/config.py
@@ -13,7 +13,7 @@ def get_env_variable(name):
 
 class Config(object):
 
-    SECRET_KEY = os.environ.get("SECRET_KEY") or "super-secret-key"
+    SECRET_KEY = os.environ.get("SECRET_KEY")
     DEBUG = True
     CSRF_ENABLED = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
@@ -22,16 +22,16 @@ class Config(object):
 class ProductionConfig(Config):
     DEBUG = False
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
-    SECRET_KEY = os.environ.get("SECRET_KEY") or "prod-secret-key"
+    SECRET_KEY = os.environ.get("SECRET_KEY")
 
 class DevelopmentConfig(Config):
     DEVELOPMENT = True
     TESTING = False
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
-    SECRET_KEY = os.environ.get("SECRET_KEY") or "dev-secret-key"
+    SECRET_KEY = os.environ.get("SECRET_KEY")
 
 class TestingConfig(Config):
     TESTING = True
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_TEST_URL")
-    SECRET_KEY = os.environ.get("SECRET_KEY") or "test-secret-key"
+    SECRET_KEY = os.environ.get("SECRET_KEY")
